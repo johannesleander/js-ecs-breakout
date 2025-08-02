0b3ba1e7795dffadaa37a36afbe3f9ac39839866
Johannes Leander <leanderjohannes@gmail.com>
2025-08-02 14:53:23 +0200

    server logs

--- server.js
+++ server.js
@@ -2,27 +2,32 @@ const server = Bun.serve({
     fetch(request) {
         // Serve index.html for root requests
         if (request.url.endsWith("/") || request.url.endsWith("/index.html")) {
+            console.log(`200 OK: ${request.url}`);
             return new Response(Bun.file("./public/index.html"));
         }
         const url = new URL(request.url);
         const path = url.pathname.slice(1); // Remove leading '/'
 
         if (path.startsWith("node_modules")) {
+            console.log(`200 OK: ${request.url}`);
             return new Response(Bun.file(path), {
                 headers: { "Content-Type": "application/javascript" }
             })
         }
 
         if (path.endsWith(".js")) {
+            console.log(`200 OK: ${request.url}`);
             return new Response(Bun.file('./public/' + path), {
                 headers: { "Content-Type": "application/javascript" }
             })
         }
 
         if (path && Bun.file(path).size > 0) {
+            console.log(`200 OK: ${request.url}`);
             return new Response(Bun.file('./public/' + path));
         }
         // Fallback: 404
+        console.log(`404 Not Found: ${request.url}`);
         return new Response("Not found", { status: 404 });
     },
     port: 3000
